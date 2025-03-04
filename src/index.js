06f27983a31a3f7c149dbbafb03310f127329893
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-03-04 19:02:43 +0100

    Restart

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import "./styles/styles.css";
-import { displayToDoList, addToDo, removeToDo, toggleComplete, sortByDate } from "./todo.js";
-
-addToDo("clean", "clean the toilet", "2025-03-08", "yes");
-addToDo("groceries", "buy toothpaste", "2025-03-05", "yes");
-addToDo("study", "study javascript", "2025-03-07", "yes");
-
-toggleComplete("study");
-
-sortByDate();
-
-displayToDoList();
