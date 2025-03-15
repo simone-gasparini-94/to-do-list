4c61f307d1c4ca82d8cea05c5a5809b0c52d14f6
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-03-15 13:27:25 +0100

    Add some styling

--- src/ui.js
+++ src/ui.js
@@ -2,6 +2,13 @@ export function addTodoContainer() {
     const todoContainer = document.querySelector(".task-container");
     const newTodo = document.createElement("div");
     newTodo.classList.add("todo");
+
+    const title = document.createElement("input");
+    title.type = "text";
+    title.placeholder = "Enter task...";
+    title.required = true;
+
+    newTodo.appendChild(title);
     todoContainer.appendChild(newTodo);
     console.log("add todo")
 }
