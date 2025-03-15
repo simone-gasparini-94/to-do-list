051cf33258d487c44083cf13fa22cb78ddb8bd39
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-03-15 13:53:13 +0100

    Still styling

--- src/ui.js
+++ src/ui.js
@@ -3,12 +3,15 @@ export function addTodoContainer() {
     const newTodo = document.createElement("div");
     newTodo.classList.add("todo");
 
+    const content = document.createElement("div");
+    content.classList.add("content-container");
+
     const title = document.createElement("input");
     title.type = "text";
-    title.placeholder = "Enter task...";
     title.required = true;
 
-    newTodo.appendChild(title);
+    content.appendChild(title)
+    newTodo.appendChild(content);
     todoContainer.appendChild(newTodo);
     console.log("add todo")
 }
